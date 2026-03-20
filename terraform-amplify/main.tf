05d43dd05ce9c0550fde5e908b6fbf049d21c893
mely200424 <al22020346@itsa.edu.mx>
2026-03-20 13:02:40 -0600

    Subida final sin archivos binarios de terraform

---
+++ terraform-amplify/main.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "github_token" {
+  description = "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"
+  type        = string
+  sensitive   = true
+}
+
+resource "aws_amplify_app" "hola-mundo" {
+  name       = "potato"
+  repository = "https://github.com/mely200424/potato"
+  oauth_token = var.github_token
+
+  build_spec = <<-EOT
+version: 1
+frontend:
+  phases:
+    preBuild:
+      commands:
+        - npm ci
+    build:
+      commands:
+        - npm run build
+  artifacts:
+    baseDirectory: dist
+    files:
+      - '**/*'
+  cache:
+    paths:
+      - node_modules/**/*
+EOT
+}
+
+resource "aws_amplify_branch" "main" {
+  app_id = aws_amplify_app.hola-mundo.id
+  branch_name = "main"
+}
+
+output "amplify_app_url" {
+  value = aws_amplify_app.hola-mundo.default_domain
+}
---
+++ terraform-amplify/terraform.tfvars
@@ -0,0 +1,1 @@
+github_token = "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"
